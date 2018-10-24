991b0198be14a833ff4912e35beeddd827ab563e
EMEA\pglad <peterglad1985@hotmail.com>
2018-10-24 10:20:23 +0200

    f

--- datarecorder.py
+++ datarecorder.py
@@ -19,11 +19,11 @@ class DataRecorder:
         self.total_beacons_per_second_cache.pop(self.game.frameCounter, None)
         if self.ts.pipe.poll():
             text = self.ts.pipe.recv()
-            list = text.split(';')
+            packet_list = text.split(';')
             self.total_packets += 1
             self.total_packets_per_second_cache[self.game.frameCounter] = ''
             self.total_packets_per_second = len(self.total_packets_per_second_cache)
-            if list[0] == 8:
+            if packet_list[0] == '8':
                 self.total_beacons += 1
                 self.total_beacons_per_second_cache[self.game.frameCounter] = ''
                 self.total_beacons_per_second = len(self.total_beacons_per_second_cache)
