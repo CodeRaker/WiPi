d5b31f94215fc59b09496c1ce8740b9563f032cb
EMEA\pglad <peterglad1985@hotmail.com>
2018-10-25 16:13:13 +0200

    d

--- datarecorder.py
+++ datarecorder.py
@@ -21,17 +21,16 @@ class DataRecorder:
         if self.ts.pipe.poll():
             text = self.ts.pipe.recv()
             packet_list = text.split(';')
+            self.total_packets += 1
+            self.total_packets_per_second_cache[self.game.frameCounter] = ''
+            self.total_packets_per_second = len(self.total_packets_per_second_cache)
 
             if packet_list[0] == '8':
-                self.total_packets += 1
-                self.total_packets_per_second_cache[self.game.frameCounter] = ''
                 self.total_beacons += 1
                 self.total_beacons_per_second_cache[self.game.frameCounter] = ''
                 self.total_beacons_per_second = len(self.total_beacons_per_second_cache)
 
             elif packet_list[0] in ['4','5']:
-                self.total_packets += 1
-                self.total_packets_per_second_cache[self.game.frameCounter] = ''
                 self.total_probes += 1
                 if packet_list[2] not in ['lichen','huron','CSCEmployee','']:
                     if packet_list[4] not in self.devices.keys():
@@ -41,4 +40,3 @@ class DataRecorder:
                         if packet_list[2] not in self.devices[packet_list[4]]:
                             self.devices[packet_list[4]].append(packet_list[2])
                             
-            self.total_packets_per_second = len(self.total_packets_per_second_cache)
