abc967108608cc73d0aca74fa0bed36d5d05b470
EMEA\pglad <peterglad1985@hotmail.com>
2018-10-29 15:03:58 +0100

    s

--- mpcmd.py
+++ mpcmd.py
@@ -88,5 +88,6 @@ class TsharkJsonController(TsharkJsonMonitor):
         while True:
             if self.monitor.monitor_control_in.poll():
                 if self.monitor.monitor_control_in.recv() == 'done':
+                    print('received confirmation')
                     break
         self.monitor.monitor.terminate()
